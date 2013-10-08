54d666ef1a5ef096761ca2d8cc97eabe642b8563
Alex <alexanderortiz.201@gmail.com>
2013-10-07 22:15:02 -0400

    added utils module and namespace function

---
+++ root/js/main.js
@@ -0,0 +1,19 @@
+// Salon namespace
+var Salon = Salon || {};
+
+Salon.utils.namespace('Salon.utils');
+
+Salon = (function (){
+  "use strict";
+  return {
+      init: function (){
+
+      }
+  };
+})();
+
+$(document).ready(function () {
+    "use strict";
+    Salon.init();
+
+});
---
+++ root/js/vendor/salon.utils.js
@@ -0,0 +1,28 @@
+var Salon = Salon || {};
+
+Salon.utils = (function(){
+    "use strict";
+    return {
+        namespace: function (ns_string) {
+
+            var parts = ns_string.split('.'),
+                parent = Salon,
+                i; // strip redundant leading global
+
+            if (parts[0] === 'Salon') {
+                parts = parts.slice(1);
+            }
+
+            for (i = 0; i < parts.length; i += 1) {
+                // create a property if it doesn't exist
+                if (typeof parent[parts[i]] === 'undefined') {
+                    parent[parts[i]] = {};
+                }
+                parent = parent[parts[i]];
+            }
+
+            return parent;
+        }
+    };
+
+})();
