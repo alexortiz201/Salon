0b6a25e0ac8aa1058346a2dde7287b0d4ee360cc
Alex <alexanderortiz.201@gmail.com>
2013-10-20 11:18:36 -0400

    file rename added router pre-alpha

--- root/js/router/router.js
+++ root/js/router/router.js
@@ -4,14 +4,18 @@ var AppRouter = Backbone.Router.extend({
         "*actions": "defaultRoute" // Backbone will try match the route above first
     }
 });
+
 // Instantiate the router
 var app_router = new AppRouter;
+
 app_router.on('route:getPost', function (id) {
     // Note the variable in the route definition being passed in here
     alert( "Get post number " + id );
 });
+
 app_router.on('route:defaultRoute', function (actions) {
     alert( actions );
+    //take to home page by default
 });
 // Start Backbone history a necessary step for bookmarkable URL's
 Backbone.history.start();
